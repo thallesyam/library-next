b26f3ca7a84237cb5bd40d4ac41a1f65cbb594d6
Thalles Ian <thallesyam@users.=.github.com>
2021-02-18 21:26:59 -0300

    reset database

--- prisma/migrations/20210218195242_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Books" DROP CONSTRAINT "Books_ownerId_fkey";
-
--- AlterTable
-ALTER TABLE "Books" ALTER COLUMN "ownerId" SET DATA TYPE TEXT;
-
--- AddForeignKey
-ALTER TABLE "Books" ADD FOREIGN KEY ("ownerId") REFERENCES "Users"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
